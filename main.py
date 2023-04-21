9c3880b4f571ab04a31fd4ef2299bcaeb46c98fe
Baullin1767 <starkov.alexd@gmail.com>
2023-04-21 22:50:20 +0000

    Version 0.1.3

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+#uvicorn main:app --reload
+
 import uuid
 from fastapi import FastAPI, Body, status
 from fastapi.responses import JSONResponse, FileResponse
+
+from db import *
  
 class Recipe:
-    def __init__(self, name, description="Описание", ingredients="ингридиенты", steps="шаги"):
+    def __init__(self, name, description="Описание", ingredients="ингридиенты", steps={"шаг": 5}):
         self.name = name
         self.description = description
         self.ingredients = ingredients
         self.steps = steps
         self.id = str(uuid.uuid4())
- 
-# условная база данных УДАЛИТЬ
-recipes = [Recipe("Творожная запеканка с яблоками и морковью"),
-        Recipe("Банановое печенье"), 
-        Recipe("Манник на кефире, с изюмом")]
- 
-# для поиска рецепта в списке recipes
-def find_recipe(id):
-    for recipe in recipes: 
-         if recipe.id == id:
-            return recipe
-    return None
+
+
+connect_db()
+
+# def create_recipe():
+#     print('тут')
+#     for _ in range(100):
+#         recipe = Recipe("name",
+#                         "description",
+#                         "ingredients",
+#                         {"шаг": 5})
+
+#         # добавляем объект в таблицу recipes
+#         db_create_recipe(recipe)
+
+
+def get_time_cooking(recipe):
+    steps_text = json.loads(recipe[4])
+    time = 0
+    for step_time in steps_text.values:
+        try:
+            time+=step_time
+        except ValueError:
+            continue
+    return time
+
  
 app = FastAPI()
  
@@ -30,12 +48,13 @@ async def main():
  
 @app.get("/api/recipes")
 def get_recipes():
-    return recipes
+    return db_get_recipes()
+
  
 @app.get("/api/recipes/{id}")
 def get_recipe(id):
     # получаем рецепт по id
-    recipe = find_recipe(id)
+    recipe = db_get_recipes_id(id)
     print(recipe)
     # если не найден, отправляем статусный код и сообщение об ошибке
     if recipe == None:  
@@ -45,42 +64,83 @@ def get_recipe(id):
         )
     #если пользователь найден, отправляем его
     return recipe
- 
+
+
+@app.get("/api/recipes/filters/{filter}")
+def get_recipes_filtered(filter):
+    if filter == "min_to_max" or filter == 'max_to_min':
+        get_recipes_min_max(filter)
+    else:
+        get_recipes_by_ingredients(filter)
+
+
+# Поиск по ингридиентам
+def get_recipes_by_ingredients(ingredient):
+    recipes = db_get_recipes()
+    recipes_with_ingredient=[]
+    for recipe in recipes:
+        if ingredient in recipe[4]:
+            recipes_with_ingredient.append()
+    if recipes_with_ingredient == []:  
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Рецепты не найден с таким ингридиентом не найдены" }
+        )
+
+# Сортируем по времени готовки
+def get_recipes_min_max(filter):
+    recipes = db_get_recipes()
+    dict_recipes={}
+    for recipe in recipes:
+        time_cooking = get_time_cooking(recipe)         # Получаем все рецепты
+        dict_recipes.update({recipe[0]: time_cooking})  # И добавляем в словарь id: время готовки
+    if filter == "min_to_max":
+        sorted(dict_recipes, key=dict_recipes.get)  
+        return dict_recipes # Возвращаем отсортированый словарь             
+    elif filter == "max_to_min":
+        sorted(dict_recipes, key=dict_recipes.get, reverse=True)
+        return dict_recipes # Возвращаем отсортированый словарь
+
+
+@app.post("/api/recipes/")
+
+
+
  
 @app.post("/api/recipes")
 def create_recipe(data  = Body()):
+    print('тут')
     recipe = Recipe(data["name"],
                     data["description"],
                     data["ingredients"],
                     data["steps"])
 
-    # добавляем объект в список recipes
-    recipes.append(recipe)
+    # добавляем объект в таблицу recipes
+    db_create_recipe(recipe)
     return recipe
  
 @app.put("/api/recipes")
 def edit_recipe(data  = Body()):
   
     # получаем рецепт по id
-    recipe = find_recipe(data["id"])
+    recipe = db_get_recipes_id(data["id"])
     # если не найден, отправляем статусный код и сообщение об ошибке
     if recipe == None: 
         return JSONResponse(
                 status_code=status.HTTP_404_NOT_FOUND, 
                 content={ "message": "Рецепт не найден" }
         )
-    # если рецепт найден, изменяем его данные и отправляем обратно клиенту
-    recipe.name = data["name"]
-    recipe.description = data["description"]
-    recipe.ingredients = data["ingredients"]
-    recipe.steps = data["steps"]
+    else:
+        # если рецепт найден, изменяем его данные и отправляем обратно клиенту
+        db_edit_recipe(data)
+        recipe = db_get_recipes_id(data["id"])
     return recipe
  
  
 @app.delete("/api/recipes/{id}")
 def delete_recipe(id):
     # получаем пользователя по id
-    recipe = find_recipe(id)
+    recipe = db_get_recipes_id(id)
   
     # если не найден, отправляем статусный код и сообщение об ошибке
     if recipe == None:
@@ -90,5 +150,5 @@ def delete_recipe(id):
         )
   
     # если пользователь найден, удаляем его
-    recipes.remove(recipe)
+    db_delete_recipe(id)
     return recipe
