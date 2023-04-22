3390165afb23a51e9bb5a1fdf38c856207b594a0
Baullin1767 <86959616+Baullin1767@users.noreply.github.com>
2023-04-23 02:47:05 +0500

    Update db.py

--- db.py
+++ db.py
@@ -16,12 +16,12 @@ def connect_db():
 
 
 #INSERT
-
+# Создаём рецепт
 def db_create_recipe(recipe):
     cursor.execute(
         "INSERT INTO recipes (id, name, description, ingredients, steps) VALUES(%s, %s, %s, %s, %s)",
         (recipe.id, recipe.name, recipe.description, recipe.ingredients, json.dumps(recipe.steps)))
-
+# Создаём пользователя
 def db_create_user(user):
     cursor.execute(
         "INSERT INTO users (id, login, password) VALUES(%s, %s, %s, %s, %s)",
@@ -29,17 +29,17 @@ def db_create_user(user):
 
 
 #SELECT
-
+# получаем рецепты
 def db_get_recipes():
     recipes = cursor.execute("SELECT * FROM recipes")
     recipes = cursor.fetchall()
     return recipes
-
+# Получаем рецепт по id
 def db_get_recipes_id(recipe_id):
     cursor.execute("SELECT * FROM recipes WHERE id=%s", (recipe_id,))
     recipe = cursor.fetchall()
     return recipe
-
+# Проверяем наличие пользователя в базе
 def db_get_user_ex(user_id):
     cursor.execute("SELECT * FROM recipes WHERE id=%s", (user_id,))
     user = cursor.fetchall()
@@ -49,7 +49,7 @@ def db_get_user_ex(user_id):
         return False
 
 #UPDATE
-
+# Редактируем рецепт
 def db_edit_recipe(data = Body()):
     cursor.execute(
             "UPDATE recipes SET name=%s WHERE id=%s", (data["name"], data["id"]))
@@ -61,6 +61,6 @@ def db_edit_recipe(data = Body()):
         "UPDATE recipes SET steps=%s WHERE id=%s",(json.dumps(data["steps"]), data["id"]))
 
 #DЕLETE
-
+# Удаляем рецепт по id
 def db_delete_recipe(id):
     cursor.execute("DELETE FROM recipes WHERE id=%s", (id,))
